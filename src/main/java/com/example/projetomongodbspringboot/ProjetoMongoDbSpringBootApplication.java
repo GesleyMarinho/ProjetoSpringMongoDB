7597d8d0c20e3b852c6d3b04353d89e24a9d7cde
GesleyMarinho <gesleymarinho@gmail.com>
2026-03-02 20:47:13 -0300

    primeiro commit

---
+++ src/main/java/com/example/projetomongodbspringboot/ProjetoMongoDbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetomongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMongoDbSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoMongoDbSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetomongodbspringboot/ProjetoMongoDbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetomongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMongoDbSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
